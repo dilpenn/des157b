55e25787b8eb15b4ec71e2d43aeca495f2078aac
Dilara Sarigoz <41348264+dilpenn@users.noreply.github.com>
2023-04-24 19:02:19 -0700

    blockbuster - fix empty src

--- studio1/script.js
+++ studio1/script.js
@@ -2,15 +2,18 @@
     'use strict';
 
     const myVideo = document.querySelector('#myVideo');
-    const source = document.querySelector('source');
-    const videos = ['media/cuddles.mp4', 'media/rolling.mp4', 'media/hug.mp4', 'media/niagara.mp4', 'media/golf.mp4', 'media/car.mp4', 'media/birthday.mp4'];
+    const sourceMP4 = document.querySelector('#source_mp4');
+    const sourceWEBM = document.querySelector('#source_webm');
+    const videosMP4 = ['media/cuddles.mp4', 'media/rolling.mp4', 'media/hug.mp4', 'media/niagara.mp4', 'media/golf.mp4', 'media/car.mp4', 'media/birthday.mp4'];
+    const videosWEBM = ['media/cuddles.webm', 'media/rolling.webm', 'media/hug.webm', 'media/niagara.webm', 'media/golf.webm', 'media/car.webm', 'media/birthday.webm'];
     const loading = document.querySelector('.fa-cake-candles');
     const text = document.querySelector('#text');
     const textCollection = ['from cuddling...', '...to synchronized rolling... ', '...to more cuddling...', '...to traveling...', '...to fencing with golf clubs...', '...to messy hair car rides...', 'Happy Birthday <i class="fa-solid fa-cake-candles" id="cake">'];
     let index = 0;
     
     //start playing birthday clip
-    source.setAttribute('src', videos[videos.length-1]);
+    sourceMP4.setAttribute('src', videosMP4[videosMP4.length-1]);
+    sourceWEBM.setAttribute('src', videosWEBM[videosWEBM.length-1]);
     myVideo.load();
     myVideo.play();
     text.innerHTML = textCollection[textCollection.length-1];
@@ -26,14 +29,15 @@
     function changeVideo() {
         console.log('video ended');
 
-        source.setAttribute('src', videos[index]); //update source of video to next in array
+        sourceMP4.setAttribute('src', videosMP4[index]); //update source of video to next in array
+        sourceWEBM.setAttribute('src', videosWEBM[index]);
         myVideo.load();
         myVideo.play();
 
         text.innerHTML = textCollection[index]; //change text when video changes
 
-        if (index >= videos.length -1) {
-            index = 0; //if at last video, stay
+        if (index >= videosMP4.length -1) {
+            index = 0; //if at last video, start over
         } else {
             index++; //otherwise keep incrementing up
         }
