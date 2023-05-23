e684350df0d37e32ec2edaf5cbb1a396225f9744
Dilara Sarigoz <41348264+dilpenn@users.noreply.github.com>
2023-05-23 13:21:16 -0700

    scrollmagic animations working

--- studio3/script.js
+++ studio3/script.js
@@ -3,21 +3,28 @@
 TweenLite.defaultEase = Linear.easeNone; //animation controls should go to scrollMagic
 
 // content images
-const content = document.querySelectorAll(".content");
+const item = document.querySelectorAll(".item");
+console.log(item);
 
 // init controller
-const controller = new ScrollMagic.Controller({vertical: false});
+const controller = new ScrollMagic.Controller();
 
 
 // timeline
 const t1 = new TimelineMax();
 
 // timeline sequence
-t1.to('#fullview', 0.5, {xPercent: -33}) //to(selector, animation duration in sec., {animation property})
-t1.from(content[1], 0.5, {opacity: 0});
+t1.to('#fullview', 1, {xPercent: -33.33}); //to(selector, animation duration in sec., {animation property})
+t1.from(item[0], 1, {right: 0}); //taxi
 
-t1.to('#fullview', 0.5, {xPercent: -66}) //to(selector, animation duration in sec., {animation property})
-t1.from(content[2], 0.5, {opacity: 0});
+t1.to('#fullview', 1, {xPercent: -33.33}); 
+t1.from(item[1], 1, {left: 0}); //bus
+
+t1.to('#fullview', 1, {xPercent: -66.66}); 
+t1.from(item[2], 1, {right: 0}); //mouse
+
+t1.to('#fullview', 1, {xPercent: -66.66}); 
+t1.from(item[3], 1.5, {right: 0}); //cheese
 
 
 //build scene
